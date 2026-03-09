ef60c9049124dff7e1e8e63a085003d79234fcaf
rchan9889 <164683407+rchan9889@users.noreply.github.com>
2026-03-08 23:02:08 -0400

    game over main menu button actually connects to main menu

--- game_over.gd
+++ game_over.gd
@@ -5,7 +5,8 @@ func _ready() -> void:
 
 func _on_retry_pressed() -> void:
 	get_tree().paused = false
-	get_tree().reload_current_scene()
+	self.hide()
+	get_tree().change_scene_to_file("res://menu.tscn")
 
 func game_over():
 	get_tree().paused = true
